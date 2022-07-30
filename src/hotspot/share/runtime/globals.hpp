4b892853a96e6d9dbfa75fd08bff146bcdd3c400
阿菌•未霜 <799620521@qq.com>
2022-07-30 18:13:17 +0800

    Runtime: Set PerBytecodeTrapLimit default value to 8

--- src/hotspot/share/runtime/globals.hpp
+++ src/hotspot/share/runtime/globals.hpp
@@ -1373,7 +1373,7 @@ const int ObjectAlignmentInBytes = 8;
           "(includes inlines)")                                             \
           range(0, max_jint)                                                \
                                                                             \
-  product(intx, PerBytecodeTrapLimit,  4,                                   \
+  product(intx, PerBytecodeTrapLimit,  8,                                   \
           "Limit on traps (of one kind) at a particular BCI")               \
           range(0, max_jint)                                                \
                                                                             \
