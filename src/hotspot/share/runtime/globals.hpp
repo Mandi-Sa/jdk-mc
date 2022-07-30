fcb1a1c1b4cbc9ce2f47ca09d9a949d940208a76
阿菌•未霜 <799620521@qq.com>
2022-07-30 18:00:50 +0800

    Runtime: Enable DisablePrimordialThreadGuardPages by default

--- src/hotspot/share/runtime/globals.hpp
+++ src/hotspot/share/runtime/globals.hpp
@@ -695,7 +695,7 @@ const int ObjectAlignmentInBytes = 8;
   product_pd(bool, DontYieldALot,                                           \
           "Throw away obvious excess yield calls")                          \
                                                                             \
-  product(bool, DisablePrimordialThreadGuardPages, false, EXPERIMENTAL,     \
+  product(bool, DisablePrimordialThreadGuardPages, true, EXPERIMENTAL,      \
                "Disable the use of stack guard pages if the JVM is loaded " \
                "on the primordial process thread")                          \
                                                                             \
