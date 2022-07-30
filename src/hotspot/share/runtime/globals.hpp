f55d5d7b2a7b6ea1dd91566e84a68758e48e60ac
阿菌•未霜 <799620521@qq.com>
2022-07-30 18:05:21 +0800

    Runtime: Set AllocatePrefetchStyle default value to 2

--- src/hotspot/share/runtime/globals.hpp
+++ src/hotspot/share/runtime/globals.hpp
@@ -1229,7 +1229,7 @@ const int ObjectAlignmentInBytes = 8;
           "When using recompilation, never interpret methods "              \
           "containing loops")                                               \
                                                                             \
-  product(intx,  AllocatePrefetchStyle, 1,                                  \
+  product(intx,  AllocatePrefetchStyle, 2,                                  \
           "0 = no prefetch, "                                               \
           "1 = generate prefetch instructions for each allocation, "        \
           "2 = use TLAB watermark to gate allocation prefetch, "            \
