4597005ea0c5a2de588e669a54a061b3ef9c473f
阿菌•未霜 <799620521@qq.com>
2022-07-30 17:30:34 +0800

    GC: Disable VerifyObjectStartArray by default

--- src/hotspot/share/gc/shared/gc_globals.hpp
+++ src/hotspot/share/gc/shared/gc_globals.hpp
@@ -555,7 +555,7 @@
   product(bool, VerifyRememberedSets, false, DIAGNOSTIC,                    \
           "Verify GC remembered sets")                                      \
                                                                             \
-  product(bool, VerifyObjectStartArray, true, DIAGNOSTIC,                   \
+  product(bool, VerifyObjectStartArray, false, DIAGNOSTIC,                  \
           "Verify GC object start array if verify before/after")            \
                                                                             \
   product(bool, DisableExplicitGC, false,                                   \
